106a56fea6bcb57908675135e7034edde2e258e0
Victoria <92251935+bnuwi@users.noreply.github.com>
2026-05-24 21:22:57 +0300

    Delete pytest_example_5 directory

--- pytest_example_5/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from calculator import Calculator
-
-@pytest.fixture
-def calc():
-    return Calculator()
